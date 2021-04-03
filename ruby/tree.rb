d932ca49210de8de11d254c13edc0da1b6bf4e6f
Carlos Chacon <carlos.rivas@smartmatic.com>
2021-04-03 14:38:00 -0500

    Redo tree.rb example. It had invalid whitespace characters that didn't allow the example to be compiled correctly.

--- ruby/tree.rb
+++ ruby/tree.rb
@@ -1,37 +1,26 @@
- ​​class Tree​​
-   # attr_accessor: defines an instance variable, an accesor and a setter.
- ​​  attr_accessor :children, :node_name​​
- ​​​​
-   # This is how a constructor is defined.
- ​​  def initialize(name, children=[])​​
-     # The @ sign denotes an instance variable. @@ denotes a class variable (one value per class).
- ​​    @children = children​​
- ​​    @node_name = name​​
- ​​  end​​
- ​​​​
- ​​  def visit_all(&block)​​
- ​​    visit &block​​
- ​​    children.each {|c| c.visit_all &block}​​
- ​​  end​​
- ​​​​
- ​​  def visit(&block)​​
- ​​    block.call self​​
- ​​  end​​
- ​​end​​
- ​​​​
- ​​ruby_tree = Tree.new( "Ruby",​​
- ​​  [Tree.new("Reia"),​​
- ​​   Tree.new("MacRuby")] )​​
- ​​​​
- ruby_tree_2 = Tree.new( "Ruby2",​​
- ​​  [Tree.new("Reia2"),​​
- ​​   Tree.new("MacRuby2")] )​​
- ​
- ​​puts "Visiting a node"​​
- ​​ruby_tree.visit {|node| puts node.node_name}​​
- ​​puts​​
- ​​​​
- ​​puts "visiting entire tree"​​
- ​​ruby_tree.visit_all {|node| puts node.node_name}​​
+class Tree
+  attr_accessor :children, :node_name
 
- puts ruby_tree_2.
+  def initialize(name, children=[])
+    @children = children
+    @node_name = name
+  end
+
+  def visit(&block)
+    block.call self
+  end
+
+  def visit_all(&block)
+    visit &block
+    children.each { |c| c.visit_all &block }
+  end
+end
+
+ruby_tree = Tree.new("Ruby", [Tree.new("Reia"), Tree.new("MacRuby")])
+
+puts "Visiting a node"
+ruby_tree.visit { |node| puts node.node_name }
+puts
+
+puts "Visiting entire tree"
+ruby_tree.visit_all { |node| puts node.node_name }
